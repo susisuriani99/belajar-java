cdc5012732d6873983e134048166602ef5595696
Made Mas Adi Winata <m45adiwinata@gmail.com>
2025-07-15 22:18:56 +0700

    ternary operator

---
+++ Belajar/TenaryOperator.java
@@ -0,0 +1,15 @@
+public class TenaryOperator {
+    public static void main(String[] args) {
+
+    var nilai = 75
+
+    if (nilai >= 75) {
+        ucapan = "selamat anda lulus";
+    } else {
+        ucapan = "anda tidak lulus";
+    }
+
+    system.out.println(ucapan);
+    
+    }
+}
