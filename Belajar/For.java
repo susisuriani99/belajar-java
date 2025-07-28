66e6176d71a350407a58a456d0c94b5701ecb9bb
Made Mas Adi Winata <m45adiwinata@gmail.com>
2025-07-28 21:53:10 +0700

    for loop

--- Belajar/For.java
+++ Belajar/For.java
@@ -21,11 +21,52 @@ public class For {
             counter2++;
         }
 
-        var counter3= 20;
+        var counter3= 1;
 
         do {
             System.out.println("Perulangan ke-" + counter3);
             counter3++;
         } while (counter3 <=20);
+
+        var counter4 = 1;
+        while (true) {
+            System.out.println("Perulangan ke-" + counter4);
+            counter4++;
+
+            if (counter4 >10) {
+                break;
+            }
+
+        }
+
+
+        for(int counter5 = 1; counter5 <=50; counter5++) {
+            if (counter5 % 2 == 0) {
+                continue;
+            }
+        System.out.println("Perulangan Ganjil " + counter5);
+        }
+
+
+        String[] array = {
+            "Susi", "Suriani", "Ryani", "Made", "Winata", "Adi"
+        };
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 }
+
+/*
+ * @
+ * @@
+ * @@@
+ * @@@@
+ * @@@@@
+ * @@@@@@
+ * @@@@@@@
+ * @@@@@@@@
+ * @@@@@@@@@
+ * @@@@@@@@@@
+ */
