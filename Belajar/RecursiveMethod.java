dfa14fe15a8e349a42da2cc87245aefb16f51800
Made Mas Adi Winata <m45adiwinata@gmail.com>
2025-08-17 20:42:43 +0700

    recursive method

--- Belajar/RecursiveMethod.java
+++ Belajar/RecursiveMethod.java
@@ -2,13 +2,26 @@ package Belajar;
 
 public class RecursiveMethod {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(factorial(5));
     }
-        static int factorial(int value) {
-            var result = 1;
-            for (int i = 1; i <= value; i++) {
-                result *= i;
-            }
+    static int factorial(int value) {
+        var result = 1;
+
+        for (var counter = 1; counter <= value; counter++) {
+            result *= counter;
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorialRecursive(10));
+    }
+
+    static int factorialRecursive(int value) {
+        if (value == 1) {
+            return 1;
+        } else {
+            return value * factorialRecursive(value-1);
+        }
+    }
 }
